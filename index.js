4ac86009b8370149b0ee6d4ba63aed8115b80282
Addie Dean <addiedean@gmail.com>
2019-04-23 16:11:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+  var instruments=['Guitar','Bass Guitar','Lead Guitar','Drums'];
+  var musicians=['John Lennon,','Paul McCartney','George Harrison', 'Ringo Starr'];
+  
+function theBeatlesPlay(musicians,instruments){
+  var empty =[]
+  for (let i=0; i < musicians.length; i++)
+  empty.push(musicians [i] + " plays " + instruments [i] )
+return empty;
+}
+
+var facts=[
+        "He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"
+        ];
+ 
+function johnLennonFacts(facts){
+  var newFacts=[]
+  let i=0
+  while(i<=facts.length){
+newFacts.push(johnLennonFacts(i)+"!!!")
+  }
+  return (newFacts[i])
+  
+
+        
+
+}
+
+  
+
+
 // add solution here
